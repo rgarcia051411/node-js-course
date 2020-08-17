a9eb0f77179bbc4548866bfee2c362d4c5dd43d3
robinson garcia <robinsongarcia@robinsons-MacBook-Pro.local>
2020-08-17 06:01:24 -0700

    fisnish working on the backend

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,28 +1,26 @@
-
-const request = require('postman-request')
+const request = require('postman-request');
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicmdhcmNpYTA1MTQxMSIsImEiOiJja2N6YWxzN3owYjhxMnZxMGM4NXEzNDdmIn0.-1iI08LSLxLH-FyeJgtAhA&limit=1'
-  
-    request({url, json: true}, (error, { body } ) => { //short hand syntax used. old(url:url) response was deconstructed to {body} orignal {response}
-       
-        if(error){
-            callback("Unable to connect to Location Services!", undefined)
-        } else if(body.features.length === 0){
-            callback('Unable to find location. Try another search', undefined)
+    const url =
+        'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+        encodeURIComponent(address) +
+        '.json?access_token=pk.eyJ1IjoicmdhcmNpYTA1MTQxMSIsImEiOiJja2N6YWxzN3owYjhxMnZxMGM4NXEzNDdmIn0.-1iI08LSLxLH-FyeJgtAhA&limit=1';
+
+    request({ url, json: true }, (error, { body }) => {
+        //short hand syntax used. old(url:url) response was deconstructed to {body} orignal {response}
+
+        if (error) {
+            callback('Unable to connect to Location Services!', undefined);
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search', undefined);
         } else {
             callback(undefined, {
                 latitude: body.features[0].center[1],
                 laongitude: body.features[0].center[0],
-                location: body.features[0].place_name
+                location: body.features[0].place_name,
             });
         }
-    })
-  };
-  
- 
+    });
+};
 
-
-  module.exports = geocode;
-
-  
+module.exports = geocode;
